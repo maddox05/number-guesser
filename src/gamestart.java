450637c09227dddf876462b81bf31fa6a1878deb
Maddox Schmidlkofer <95244098+Maddox05@users.noreply.github.com>
2023-02-05 17:16:08 -0500

    Update gamestart.java

--- src/gamestart.java
+++ src/gamestart.java
@@ -1,24 +1,38 @@
+import java.util.Scanner;
 import java.util.Random;
 public class gamestart
 {
     private int maxnumber;
     private int enterednumber;
+    Random rand = new Random();
+    private int randomnumber;
+    Scanner myObj = new Scanner(System.in);
 
     public gamestart(int max, int usernum)
     {
         maxnumber = max;
         enterednumber = usernum;
-
+        int randomnumber = rand.nextInt(maxnumber+1);
     }
     public void gameisgoing(){
-        Random rand = new Random();
         if (enterednumber <= maxnumber){
-            int randomnumber = rand.nextInt(maxnumber+1);
+
             if(randomnumber == enterednumber){
                 System.out.println("you guessed correctly! The Random Number was: " + randomnumber);
             }
             else{
-                System.out.println("you guessed Incorrectly. The Random Number was: " + randomnumber);
+                if(enterednumber < randomnumber){
+                    System.out.println("the Random Number is greater than what you guessed, please guess again");
+                    System.out.println("Enter your New guess: ");
+                    enterednumber = Integer.parseInt(myObj.nextLine());
+                    gameisgoing();
+                }
+                else{
+                    System.out.println("the Random Number is less than what you guessed, please guess again");
+                    System.out.println("Enter your New guess: ");
+                    enterednumber = Integer.parseInt(myObj.nextLine());
+                    gameisgoing();
+                }
             }
 
         }
